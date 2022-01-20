10e85ebe91a275d79a2ca9ea286ea709d154e8ef
Tcheago <thiago.correa2@estudante.ifms.edu.br>
2022-01-20 18:02:09 -0400

    Layout quase responsivo (not finished)

--- src/components/container/requestComponents/SubContainerOne.js
+++ src/components/container/requestComponents/SubContainerOne.js
@@ -4,19 +4,23 @@ import Name from './SubContainerOneComponents/Name';
 import Login from './SubContainerOneComponents/Login';
 import Bio from './SubContainerOneComponents/Bio';
 import Joined from "./SubContainerOneComponents/Joined";
-
+//<Bio repositories={props.repositories}/>
 export default function SubContainerOne(props){
     return(
-        <div className="subContainerOne">
-            <div className="nameLoginAndAvatar">
+        <>
+            <div className="subContainerOne">
                 <Avatar repositories={props.repositories}/>
-                <div className="nameLogin">
-                    <Name repositories={props.repositories}/>
-                    <Login repositories={props.repositories}/>
-                    <Bio repositories={props.repositories}/>
+                <div className="nameLoginAndJoined">
+                    <div className="nameLogin">
+                        <Name repositories={props.repositories}/>
+                        <Login repositories={props.repositories}/>
+                    </div>
+                        <Joined repositories={props.repositories}/>
                 </div>
-                <Joined repositories={props.repositories}/>
             </div>
-        </div>
+            <div className="bio">
+                <Bio repositories={props.repositories}/>
+            </div>
+        </>
     );
 }
--- src/components/container/requestComponents/SubContainerOneComponents/Bio.js
+++ src/components/container/requestComponents/SubContainerOneComponents/Bio.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Bio(props){
     const bioo = () => {
         if(props.repositories.bio == null){
-            return <p className="span bioMargin">This profile has no bio</p>
+            return <p className="bioSpan">This profile has no bio</p>
         }else if(props.repositories.bio.length < 45){
-            return <p className="span bioMargin">{props.repositories.bio}</p>
+            return <p className="bioSpan">{props.repositories.bio}</p>
         }else{
-            return <p className="span bioMargin">The bio is too big</p>
+            return <p className="bioSpan">The bio is too big</p>
         }
     }
 

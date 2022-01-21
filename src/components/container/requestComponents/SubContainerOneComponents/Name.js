80a33f5f1ea97e6ae27827e93bf68210081b9fa3
Tcheago <thiago.correa2@estudante.ifms.edu.br>
2022-01-20 23:31:24 -0400

    Responsive and adjustments

--- src/components/container/requestComponents/SubContainerOneComponents/Name.js
+++ src/components/container/requestComponents/SubContainerOneComponents/Name.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 export default function Name(props){
+    const width = window.screen.width;
+
     const name = () => {
         if(props.repositories.name == null){
-            return <h1>Not Available</h1>
+            return <h1>Unnamed</h1>
         }else if(props.repositories.name.length < 20){
-            return <h1>{props.repositories.name}</h1>
+            if(width < 700 && props.repositories.name.includes(' ')){
+                const compressed = props.repositories.name.split(" ");
+
+                return <h1>{compressed[0]}</h1>
+            }else{
+                return <h1>{props.repositories.name}</h1>
+            }
         }else{
             const initialsValues = props.repositories.name.split(" ");
+            
             return <h1>{initialsValues[0]}</h1>
         }
     }
